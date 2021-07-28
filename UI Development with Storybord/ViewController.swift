eaeb338c6cb35553eff18bb49ac7e3a1cc80d38e
Ruba <RubaAlmutairi@gmail.com>
2021-07-28 18:02:05 +0300

    finch coding all

--- UI Development with Storybord/ViewController.swift
+++ UI Development with Storybord/ViewController.swift
@@ -1,19 +1,21 @@
-//
-//  ViewController.swift
-//  UI Development with Storybord
-//
-//  Created by Ruba Almutairi on 18/12/1442 AH.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        textField.becomeFirstResponder()
     }
 
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        label.text = "Hello \(textField.text!) !"
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textField.resignFirstResponder()
+    }
 }
 
